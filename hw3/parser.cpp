3ec4448e080ba22df9ac875008710a5b6caffedb
Alfonso Rojas <alfonsar@usc.edu>
2018-09-25 02:18:41 -0700

    work in progress

--- hw3/parser.cpp
+++ hw3/parser.cpp
@@ -1,13 +1,23 @@
-#include <iosteam>
+#include <iostream>
 #include <ostream>
 #include <fstream>
 #include "stackint.h"
 #include "circular_list_int.h"
 
+void print (StackInt &stack)
+{
+	while(!stack.empty())
+	{
+		std::cout<<stack.top()<<std::endl;
+		stack.pop();
+	}
+	return;
+}
+
 
 int main(int argc, char* argv[])
 {
-	if(argc<3)
+	if(argc<2)
 	{
 		return -1;
 	}
@@ -23,13 +33,11 @@ int main(int argc, char* argv[])
 	while(getline(infile,s_line))
 	{
 		bool good=false;
-		//if emptty, we move on
-	
 		if(s_line.size()==0)
 		{
 			continue;
 		}
-		Stack syntax;
+		StackInt syntax;
 		for(int i=0;i<s_line.size();i++)
 		{
 			if(s_line[i]=='(')
@@ -41,108 +49,127 @@ int main(int argc, char* argv[])
 				syntax.pop();
 				//continue;
 			}
-			if(syntax.size()==0)
+			if(syntax.empty())
 			{
 				good=true;
 			}
 		}
-			if(good)
+		//if(good)
+		//{	
+			StackInt mystack;
+			for(int i=0;i<s_line.size();i++)
 			{
-				Stack mystack;
-				for(int i=0;i<s_line.size();i++)
+				if(s_line[i]==' ') continue;
+				else if(s_line[i]=='(') mystack.push(OPEN_PAREN);
+				//if(s_line[i]==')') mystack.push(CLOSED_PAREN);
+				else if(s_line[i]=='*') mystack.push(MULT);
+				else if(s_line[i]=='+') mystack.push(PLUS);
+				else if(s_line[i]=='<') mystack.push(LEFT);
+				else if(s_line[i]=='>') mystack.push(RIGHT);
+				else if(s_line[i]==')')
 				{
-					if(s_line[i]==' ') continue;
-					if(s_line[i]=='(') mystack.push(OPEN_PAREN);
-					//if(s_line[i]==')') mystack.push(CLOSED_PAREN);
-					if(s_line[i]=='*') mystack.push(MULT);
-					if(s_line[i]=='+') mystack.push(PLUS);
-					if(s_line[i]=='<') mystack.push(LEFT);
-					if(s_line[i])=='>') mystack.push(RIGHT);
-					if(s_line[i]>-1) mystack.push(s_line);
-					
-				}
-
-				int n=1;
-				int numbers=0;
-				bool begin=false;
-				Stack calc;
-				while(mystack.top()!=OPEN_PAREN)
-				{
-					while(mystack.top()>-1 && mystack.size()!=0)
+					int n=1;
+					int number=0;
+					bool begin=false;
+					while(mystack.top()!=OPEN_PAREN)
 					{
-						number+=mystack.top()*n;
-						n*=10;
-						begin=true;
-						mystack.pop();
-
-					}
-					if(mystack.top()==LEFT || mystack.top()==RIGHT )
-					{
-						if (!begin) 
+						while(mystack.top()>-1 && !mystack.empty())
 						{
-							good=false;
-						}
-						while(mystack.top()==LEFT)
-						{
+							number+=mystack.top()*n;
+							n*=10;
+							begin=true;
 							mystack.pop();
-							number*=2;
+							std::cout<<number<<std::endl;
 						}
-						while(mystack.top()==RIGHT)
-						{
-							mystack.pop();
-							number/=2;
-						}
-					}
-					calc.push(number);
-					
-					if(mystack.top()==MULT || mystack.top()==PLUS)
-					{
-						calc.push(mystack.top());
-						mystack.pop();
-					}
-				}
-				int oper=0;
-				mystack.pop(); //pop open paren
-				while(!calc.empty())
-				{
-					if(calc.size()==1)
-					{
-						good=false;
-					}
-					int topper=calc.top();
-					calc.pop();
-					if(calc.top()==MULT)
-					{
-						oper++;
-						//check to see if two operands are together
-						if(oper<2)
-						{
-							calc.pop();
-							topper*=calc.top();
-						}
-						else good=false;
 						
-					}
-					if(calc.top()==ADD)
-					{
-						oper++;
-						//check to see if two operands are together
-						if(oper<2)
+						if(mystack.top()==LEFT || mystack.top()==RIGHT )
 						{
-							calc.pop();
-							topper+=calc.top();
+							if (!begin) 
+							{
+								good=false;
+								return -3;
+							}
+							while(mystack.top()==LEFT)
+							{
+								mystack.pop();
+								number*=2;
+							}
+							while(mystack.top()==RIGHT)
+							{
+								mystack.pop();
+								number/=2;
+							}
 						}
-						else good=false;
-					}
-					mystack.push(topper)
-					calc.pop();
+						StackInt calc;
+						calc.push(number);
+						if(mystack.top()==MULT || mystack.top()==PLUS)
+						{
+							calc.push(mystack.top());
+							mystack.pop();
+						}
+						int oper_m=0;
+						int oper_p=0;
+						mystack.pop(); //pop open paren
+						int topper=0;
+						while(!calc.empty())
+						{
+							if(calc.size()==1)
+							{
+								good=false;
+							}
+							topper=calc.top();
+							calc.pop();
+							if(calc.top()==MULT)
+							{
+								oper_m++;
+								//check to see if two operands are together
+								if(oper_p==0)
+								{
+									calc.pop();
+									topper*=calc.top();
+								}
+								else {good=false;}
+								
+							}
+							if(calc.top()==PLUS)
+							{
+								oper_p++;
+								//check to see if two operands are together
+								if(oper_m==0)
+								{
+									calc.pop();
+									topper+=calc.top();
+								}
+								else {good=false;}
+							}
+							
+							//calc.pop();
+						}
+						mystack.push(topper);
+					}	
+				}
+				else
+				{
+					int num=s_line[i]-'0';
+					mystack.push(num);
 				}
 			}
-			if (!good)
-			{
-				std::cout<<"Malformed";
-			}
-
-		
+		std::cout<<mystack.top()<<std::endl;
+		//}	
+		if(!good)
+		{
+			std::cout<<"Malformed"<<std::endl;
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
