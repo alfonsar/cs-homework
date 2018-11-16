60a3e4116aa50df23f280fe20050c635d54d2e72
Alfonso Rojas <alfonsar@usc.edu>
2018-11-15 22:49:40 -0800

    update2

--- hw7/rotateBST.h
+++ hw7/rotateBST.h
@@ -12,7 +12,8 @@ class rotateBST: public BinarySearchTree<Key, Value>{
         bool sameKeys(const rotateBST& t2) const;
         void transform(rotateBST& t2) const;
         void InOrder(std::vector<int>& result, Node<Key,Value>* start); 
-        void recursive(Node<Key,Value>* root);
+        void rrAll(Node<Key,Value>* root);
+        void makethemEqual(Node<Key,Value>* ourRoot, Node<Key,Value>* c_root);
     protected:
         void leftRotate(Node<Key, Value>* c);
         void rightRotate(Node<Key, Value>* c);
@@ -21,84 +22,72 @@ class rotateBST: public BinarySearchTree<Key, Value>{
 template<typename Key, typename Value>
 void rotateBST<Key,Value>::leftRotate(Node<Key,Value>* c)
 {
-   bool root;
-   bool child;
     //when r has no children
     if(c->getRight()==NULL)
     {
         return;
     }
+    //b is now pointing to c's right child
     Node<Key,Value>* b=c->getRight();
-    Node<Key,Value>* t2= b->getLeft();
-    b->setLeft(c);
-    if(root)
+   if(c->getParent()!=NULL)
+   {
+       if(c->getKey()<c->getParent()->getKey())
+       {
+           c->getParent()->setLeft(b);
+       }
+       else
+       {
+           c->getParent()->setRight(b);
+       }
+   }
+   else
+   {
+       this->mRoot=b;
+   }
+    c->setRight(b->getLeft());
+    //if b has a left child meaning not null
+    //then we set the parent pointer 
+    //now pointing to b
+    if(b->getLeft()!=NULL)
     {
-        b->setParent(NULL);
+        b->getLeft()->setParent(c);
     }
-    else if (!root)
-    {
-        b->setParent(c->getParent());
-        if(child)
-        {
-            b->getParent()->setLeft(c);
-        }
-        else if(!child)
-        {
-            c->getParent()->setRight(b);
-        }
-    }
+    //now adjust pointer to make c's
+    //parent b
     c->setParent(b);
-    if(t2!=NULL)
-    {
-        t2->setParent(c);
-        c->setRight(t2);
-    }
-  
+    b->setLeft(c);
 }
 template<typename Key, typename Value>
 void rotateBST<Key,Value>::rightRotate(Node<Key,Value>* c)
 {
-   bool root;
-   bool child;
-    //if c has no left child, then it should do nothing
-    if(c->getLeft==NULL)
-    {
-        return;
-    }
-    if(c->getParent()==NULL)
-    {
-        root=true;
-    }
-    else if(c->getKey()<c->getParent()->getKey())
-    {
-        child=true;
-    }
-    Node<Key,Value>* b=c->getLeft();
-    Node<Key,Value>* t2=b->getRight();
-    //working on b node
-    b->setRight(c);
-    if(root)
-    {
-        b->setParent(NULL);
-    }
-    else
-    {
-        b->setParent(c->getParent());
-        if(child)
-        {
-            b->getParent()->setRight(b);
-        }
-        else{
-            c->getParent()->setRight(b);
-        }
-    }
-    //working on c node
-    c->setParent(b);
-    if(t2!=NULL)
-    {
-        t2->setParent(c);
-        c->setLeft(t2);
-    }
+   if(c->getLeft()==NULL)
+   {
+       return;
+   }
+   Node<Key,Value>*b=c->getLeft();
+   b->setParent(c->getParent());
+   if(c->getParent()!=NULL)
+   {
+       if(c->getKey()<c->getParent()->getKey())
+       {
+           c->getParent();
+       }
+       else
+       {
+           c->getParent()->getRight(b);
+       }
+   }
+   else
+   {
+       this->mRoot=b;
+   }
+   c->setLeft(b->getRight);
+   if(b->getRight()!=NULL)
+   {
+       b->getRight()->setParent(c);
+   }
+   c->setParent(b);
+   b->setRight(c);
 }
 template<typename Key, typename Value>
 void rotateBST<Key,Value>::InOrder(std::vector<int>& result, Node<Key,Value>* start) 
@@ -135,32 +124,60 @@ void rotateBST<Key,Value>::transform(rotateBST& t2) const
     }
     Node<Key,Value>* root=t2.mRoot;
     //right rotation
-
     while(root->getLeft()!=NULL)
     {
         t2.rightRotate(root);
         root=root->getParent();
-        if(root->getLeft()==NULL && root->getRight()!=NULL)
-        {
-            root=root->getRight();
-        }
-        else if(root->getLeft()==NULL && root->getRight()==NULL)
-        {
-            break;
-        }
     }
-    while(root->getParent()!=NULL)
-    {
-        root=root->getParent();
-    }
-    while(this->mRoot->getKey()!=root->getKey())
+    t2.rrAll(root);
+    //after rrALL is called, the root pointer
+    //is pointing to null
+    while(root!=this->mRoot)
     {
         t2.leftRotate(root);
         root=root->getParent();
     }
+    Node<Key,Value>* correctLeft=this->mRoot->getLeft();
+    Node<Key,Value>* correctRight=this->mRoot->getRight();
+    t2.makethemEqual(t2.mRoot->getLeft(), correctLeft);
+    t2.makethemEqual(t2.mRoot->getRight(),correctRight);
+}
+
+
+template<typename Key, typename Value>
+void rotateBST<Key,Value>::rrAll(Node<Key,Value>* root)
+{
+   if(root==NULL) return;
+   while(root->getLeft()!=NULL)
+   {
+       this->rightRotate(root);
+       root=root->getParent();
+   }
+   rrAll(root->getRight());
 }
 template<typename Key, typename Value>
-void rotateBST<Key,Value>::recursive(Node<Key,Value>* root)
+void rotateBST<Key,Value>::makethemEqual(Node<Key,Value>* ourRoot, Node<Key,Value>* c_root)
 {
-    
+    if(ourRoot==NULL && c_root==NULL)
+    {
+        return;
+    }
+    if(ourRoot->getKey()<c_root->getKey())
+    {
+        while(ourRoot->getKey()!=c_root->getKey())
+        {
+            this->leftRotate(ourRoot);
+            ourRoot=ourRoot->getParent();
+        }
+    }
+    else if(ourRoot->getKey()>c_root->getKey())
+    {
+        while(ourRoot->getKey()!=c_root->getKey())
+        {
+            this->rightRotate(ourRoot);
+            ourRoot=ourRoot->getParent();
+        }
+    }
+    makethemEqual(ourRoot->getLeft(),c_root->getLeft());
+    makethemEqual(ourRoot->getRight(),c_root->getRight());
 }
