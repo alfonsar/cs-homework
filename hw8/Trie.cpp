50fd72c440afcbb630e22a1da2e23d3ab7a0b914
Alfonso Rojas <alfonsar@usc.edu>
2018-12-01 00:20:34 -0800

    segfault

--- hw8/Trie.cpp
+++ hw8/Trie.cpp
@@ -5,41 +5,38 @@
 #include<set>
 #include <iostream>
 
+
 TrieSet::TrieSet()
 {
     TrieNode* ourRoot= new TrieNode;
-    ourRoot->inSet=false;
-    ourRoot->letter='&';
+    ourRoot->inSet=0;
     ourRoot->parent=NULL;
     root=ourRoot;
     for(int i=0; i<26;i++)
     {
     	ourRoot->children[i]=NULL;
     }
+    ourRoot->letter='%';
 }
 TrieSet::~TrieSet()
 {
  	clear(root);
 }
 
-bool TrieSet::hasChildren(TrieNode* root)
+bool TrieSet::childrenExist(TrieNode* root)
 {
     for(int i=0; i<26; i++)
     {
-        if(root->children[i]!=NULL)
+        if(root->children[i])
         {
-            return true;
+            return 1;
         }
     }
-    return false;
+    return 0;
 }
-void TrieSet::makeChildren(TrieNode* root)
+void TrieSet::produceOffSpring(TrieNode* root)
 {
-    for(int i=0; i<26; i++)
-    {
-        root->children[i]=NULL;
-        
-    }
+    for(int i=0; i<26; i++){root->children[i]=NULL;}
 }
 /* Add this string to the set.
 Do nothing if the string is already in the set. */
@@ -59,7 +56,7 @@ void TrieSet::insert(std::string input)
         //with corresponding letter location 
         trav=trav->children[loc];
         //if at a node that has a slash
-        if(trav==NULL)
+        if(!trav)
         {
             TrieNode* newNode= new TrieNode;
             //have trav point to new node
@@ -70,7 +67,7 @@ void TrieSet::insert(std::string input)
             //we change node's letter 
             trav->letter=input[i];
             //will then allocate another 26 children 
-            makeChildren(trav);
+            produceOffSpring(trav);
         }
         else
         {
@@ -80,7 +77,7 @@ void TrieSet::insert(std::string input)
     }
     //when we are all done, we set inSet to true
     //to show it is end of word
-    trav->inSet=true;
+    trav->inSet=1;
 
 }
 void TrieSet::remove(std::string input)
@@ -89,19 +86,18 @@ void TrieSet::remove(std::string input)
     std::transform(input.begin(),input.end(),input.begin(),::tolower);
     //will create a pointer traverse through the tree
     TrieNode* trav=prefix(input);
-
     //since we are deleting, set inSet to false
-    trav->inSet=false;
+    trav->inSet=0;
     //if the node that we are at has no children,
     //we will deallocate all its children
     //then we set that node to '/'
-    while(!hasChildren(trav))
+    while(!childrenExist(trav))
     {
       	//grab the location of letter 
        	int loc=trav->letter-97;
        	//if we reach root, break
        	//because we do not delete root
-       	if(trav->letter=='&')
+       	if(trav->letter=='%')
        	{
        		break;
        	}
@@ -123,10 +119,10 @@ void TrieSet::remove(std::string input)
         trav=parental;
     }
 }
-TrieNode* TrieSet::prefix(std::string px)
+TrieNode* TrieSet::prefix(std::string px) const
 {
     //make the input lowercase
-    std::transform(px.begin(),px.end(),px.begin(),::tolower);
+    //std::transform(px.begin(),px.end(),px.begin(),::tolower);
     //will create a pointer traverse through the tree
     TrieNode* trav=root;
   	//grab the length of input
@@ -139,7 +135,7 @@ TrieNode* TrieSet::prefix(std::string px)
         trav=trav->children[loc];
         //will traverse until 
         //we reach end of the word
-        if(trav==NULL)
+        if(!trav)
         {
             //if we reach null, then that means
             //word does not exist and will return null
@@ -171,7 +167,7 @@ void TrieSet::clear(TrieNode* node)
 	//will be called in destructor
 	for(int i=0; i<26; i++)
 	{	
-		if (node->children[i] != nullptr) 
+		if (node->children[i]) 
 		{
 			clear(node->children[i]);
 		}
