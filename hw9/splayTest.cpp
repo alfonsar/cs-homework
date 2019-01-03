4e3c00a2b467689456e84bd2fc24e2c27876ebf8
Alfonso Rojas <alfonsar@usc.edu>
2019-01-02 22:54:27 -0800

    wed

--- hw9/splayTest.cpp
+++ hw9/splayTest.cpp
@@ -4,11 +4,29 @@
 using namespace std;
 int main()
 {
-	std::cout<<"hello"<<std::endl;
 	splayTree<int,int> tree;
-	//tree.insert(make_pair(1, 1));
-	tree.insert(make_pair(0, 1));
+	tree.insert(make_pair(1, 1));
+	tree.insert(make_pair(2, 1));
+	tree.insert(make_pair(4, 1));
 	tree.insert(make_pair(5, 1));
+	tree.insert(make_pair(6, 1));
+	tree.insert(make_pair(8, 1));
+	tree.insert(make_pair(7, 1));
+	tree.insert(make_pair(3, 1));
+	//tree.findMax();
+	//tree.findMax();
+	//tree.findMax();
+	//tree.findMin();
+	tree.find(6);
+	//tree.remove(2);
+	//tree.remove(3);
+	//tree.remove(5);
+	//tree.deleteMinLeaf();
+	//tree.deleteMaxLeaf();
 	tree.print();
 	return 0;
 }
+
+//Work on remove when you remove 7, splaying 6 works in that it is root but not the children
+//when you find, nothing is splayed
+//findMin isnt working correctly ()
